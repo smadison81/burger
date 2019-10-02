d578e3f4fbe0e31ce8790d60af74dee875d20e38
Stephen Madison <stephen.d.madison@gmail.com>
2019-10-01 21:12:16 -0500

    More Commits

--- config/connection.js
+++ config/connection.js
@@ -10,11 +10,8 @@ if (process.env.JAWSDB_URL) {
     password: "password",
     database: "burgers_db"
   });
-}
+};
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log(`connected as id: ${connection.threadId}`);
-});
+connection.connect();
 
 module.exports = connection;
