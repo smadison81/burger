56c9c6944fd4220262dbfc37131136f8949eeb82
Stephen Madison <stephen.d.madison@gmail.com>
2019-10-01 21:32:43 -0500

    More Commits

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,17 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
+// if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-  });
-};
+// } 
+// else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "password",
+//     database: "burgers_db"
+//   });
+// };
 
 connection.connect();
 
