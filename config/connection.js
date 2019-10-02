393d37f6f13ba1aa2df8c3ff8193d10e166459be
Stephen Madison <stephen.d.madison@gmail.com>
2019-10-01 20:51:15 -0500

    adding heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+var mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+}
 
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'burgers_db'
-})
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log(`connected as id: ${connection.threadId}`);
+});
 
-connection.connect(function(err){
-    if(err)throw err;
-    console.log(`connected as id: ${connection.threadId}`);
-})
-
-module.exports = connection
+module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 burger_name varchar(255) NOT NULL,
 devoured BOOLEAN DEFAULT false
 );
